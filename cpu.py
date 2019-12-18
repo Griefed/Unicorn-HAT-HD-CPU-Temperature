0d6877bbcaec1daefedb813fc9c6f9b985dc1fdd
Griefed <44273438+Griefed@users.noreply.github.com>
2019-12-18 17:57:53 +0100

    Colour depends on temperature
    Optimizations and clean-up

--- cpu.py
+++ cpu.py
@@ -23,8 +23,6 @@ text_y = 0
 font_file, font_size = FONT
 font = ImageFont.truetype(font_file, font_size)
 degree_sign= u'\N{DEGREE SIGN}'
-#warning = 55
-
 
 try:
 	while True:
@@ -35,23 +33,20 @@ try:
 		image = Image.new('RGB', (text_width, max(height, text_height)), (0, 0, 0))
 		draw = ImageDraw.Draw(image)
 		draw.text((text_x, text_y), TEXT, fill=(255, 255, 255), font=font)
-		check = int(cpu.temperature)
-		for prolong in range(text_width - width): #Update every couple of seconds instead of several times per second
+		if int(cpu.temperature) < 30.0:
+			hue = 0.5
+		elif int(cpu.temperature) >= 30.0 and int(cpu.temperature) < 35.0:
+			hue = 0.4
+		elif int(cpu.temperature) >= 35.0 and int(cpu.temperature) < 40.0:
+			hue = 0.3
+		elif int(cpu.temperature) >= 40.0 and int(cpu.temperature) < 45.0:
+			hue = 0.2
+		elif int(cpu.temperature) >= 45.0 and int(cpu.temperature) < 50.0:
+			hue = 0.1
+		elif int(cpu.temperature) >= 55.0:
+			hue = 0.0
+		for prolong in range(60): #Update every couple of seconds instead of several times per second
 			for x in range(width):
-				if check < 30:
-					hue = 0.5
-				if 30 <= check and check < 35:
-					hue = 0.4
-				if 35 <= check and check < 40:
-					hue = 0.3
-				if 40 <= check and check < 45:
-					hue = 0.2
-				if 45 <= check and check < 50:
-					hue = 0.1
-				if 50 <= check and check < 55:
-					hue = 0.08333333
-				if check >= 55:
-					hue = 0.0
 				br, bg, bb = [int(n * 255) for n in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
 				for y in range(height):
 					pixel = image.getpixel((x, y))
